920b0290160928e7f700a697d501c1c0cfac12ae
Seth <keeferseth132@gmail.com>
2015-10-29 10:18:30 -0600

    method

--- client/login/flowchat.js
+++ client/login/flowchat.js
@@ -1,3 +1,5 @@
+// Messages = new Meteor.Collection('messages');
+
 if (Meteor.isClient) {
 	// console.log("script is loading")
     Template.loginPage.events({
@@ -22,9 +24,50 @@ if (Meteor.isClient) {
     });
 
     Template.chatPage.events({
-    	'click .logout': function(event){
+    	'click #logout': function(event){
     		event.preventDefault();
     		Meteor.logout();
-    	}
+    	},
+
+        'submit form': function(event){
+            event.preventDefault();
+            console.log(Meteor.user());
+            if (Meteor.user()){
+              var name = Meteor.user().username;
+            } else {
+              var name = 'Annoymous';
+            }
+
+            var message = document.getElementById('message');
+            // console.log(message);
+            if (message.value !== '') {
+              Messages.insert({
+                name: name,
+                message: message.value,
+                time: Date.now()
+              })
+
+              document.getElementById('message').value = '';
+              message.value = '';
+            };
+          },
+
+          // 'submit form': function (event) {
+          //   event.preventDefault();
+          //   var postContent = $('.postContent').val();
+          //   Meteor.call('newPost', {postContent: postContent});
+          // }
     });
-}
+
+    Template.messages.helpers({
+      messages: function(){
+        return Messages.find({}, {sort: {time: -1}} );
+      }
+    });
+
+//     Meteor.methods({
+//       newPost: function (post) {
+//         Posts.insert(post);
+//       }
+//     })
+// }
